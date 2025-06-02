1766851e95ca123b716644e30603dc72d07c0e39
QBos07 <qubos@outlook.de>
2025-06-02 13:23:20 +0000

    tune touch a bit
    Signed-off-by: QBos07 <qubos@outlook.de>

--- boards/base/HHK/gmouse_lld_MCU_board.h
+++ boards/base/HHK/gmouse_lld_MCU_board.h
@@ -17,9 +17,9 @@
 #include <sdk/os/input.h>
 
 // Resolution and Accuracy Settings
-#define GMOUSE_MCU_PEN_CALIBRATE_ERROR		3
-#define GMOUSE_MCU_PEN_CLICK_ERROR			1
-#define GMOUSE_MCU_PEN_MOVE_ERROR			1
+#define GMOUSE_MCU_PEN_CALIBRATE_ERROR		1
+#define GMOUSE_MCU_PEN_CLICK_ERROR			0
+#define GMOUSE_MCU_PEN_MOVE_ERROR			0
 #define GMOUSE_MCU_FINGER_CALIBRATE_ERROR	3
 #define GMOUSE_MCU_FINGER_CLICK_ERROR		5
 #define GMOUSE_MCU_FINGER_MOVE_ERROR		3
@@ -27,6 +27,11 @@
 #define GMOUSE_MCU_Z_MAX					1			// The maximum Z reading
 #define GMOUSE_MCU_Z_TOUCHON				1			// Values between this and Z_MAX are definitely pressed
 #define GMOUSE_MCU_Z_TOUCHOFF				0			// Values between this and Z_MIN are definitely not pressed
+
+#undef GMOUSE_VFLG_POORUPDOWN
+#define GMOUSE_VFLG_POORUPDOWN 0
+#undef GMOUSE_VFLG_ONLY_DOWN
+#define GMOUSE_VFLG_ONLY_DOWN 0
 
 // How much extra data to allocate at the end of the GMouse structure for the board's use
 #define GMOUSE_MCU_BOARD_DATA_SIZE			0
